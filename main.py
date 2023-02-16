f8d798c9ef83d10c24c5c3bc2c230964544b1666
rohitjoshi21 <joshi.rk11166@gmail.com>
2023-02-16 07:40:05 +0545

    command-line option added

--- main.py
+++ main.py
@@ -3,61 +3,71 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import urllib.request as request
 from time import sleep
+import sys
+import os
 
-brave_path = "/usr/bin/brave-browser"
+
+def get_url_from_args():
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        url = "https://www.facebook.com/photo?fbid=139334082361780&set=a.130296589932196"
+    return url
+        
+    
 waitTime = 10
-# userdatadir = "/home/cantor/.config/BraveSoftware/Brave-Browser/Default/"
+
+#This code is written for brave-browser, but can be used for any browser with some minor edit on this part
+browser_path = "/usr/bin/brave-browser"
 
 op = webdriver.ChromeOptions()
-op.binary_location = brave_path
-# op.add_argument(f"--user-data-dir={userdatadir}")
+op.binary_location = browser_path
 
 driver = webdriver.Chrome(options=op)
 driver.implicitly_wait(waitTime)
 
 
-
-URL = "https://www.facebook.com/photo/?fbid=467126898842768&set=pcb.467127448842713"
-
+    
+URL = get_url_from_args()
+print("URL: ",URL)
 driver.get(URL)
 
 sleep(2)
 
 
-print(driver.current_url)
-
+directory = "downloaded_photos"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
 urls = []
+
+#Photo download limit, you can change this if you have more photos in the given post
 limit = 30
+
 count = 0
 while count < limit:
-    # sleep(2)
     myPhoto = driver.find_element(By.CLASS_NAME, 'x4fas0m')
     imgurl = myPhoto.get_attribute('src')
     if imgurl in urls:
         break
-    request.urlretrieve(imgurl, f"download_photos/photo{count}.jpg")
+    request.urlretrieve(imgurl, f"{directory}/photo{count}.jpg")
     print("Photo saved: ",count)
     urls.append(imgurl)
 
-
+	#Searching for next button
     while True:
         try:
             myDiv = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div')
             myDiv.click()
             break
         except:
-            print("error while clicking")
+            print("error occured while clicking, retrying...")
             continue
 
     sleep(1)
     count += 1
     
-for i,url in enumerate(urls):
-    request.urlretrieve(url, f"download_photos/photo{i}.jpg")
-
 print("Photos saved successfully")
-a = input("Enter a key to exit")
-sleep(2)
 
 
 
